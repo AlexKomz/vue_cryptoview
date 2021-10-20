7cee5f34c43a8b288cb0d481ed6159bf4392f98f
KVC\komzoltcev <komzoltcev.a@gmail.com>
2021-10-20 16:26:39 +0300

    Вынес график в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -176,46 +176,15 @@
         </dl>
         <hr class="w-full border-t border-gray-600 my-4" />
       </template>
-      <section class="relative" v-if="selectedTicker">
-        <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          {{ selectedTicker.name }} - USD
-        </h3>
-        <div
-          class="flex items-end border-gray-600 border-b border-l h-64"
-          ref="graph"
-        >
-          <div
-            v-for="(bar, index) in normalizedGraph"
-            :key="index"
-            :style="{ height: `${bar}%` }"
-            class="bg-purple-800 border w-10"
-          ></div>
-        </div>
-        <button
-          @click="selectedTicker = null"
-          type="button"
-          class="absolute top-0 right-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            x="0"
-            y="0"
-            viewBox="0 0 511.76 511.76"
-            style="enable-background: new 0 0 512 512"
-            xml:space="preserve"
-          >
-            <g>
-              <path
-                d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-                fill="#718096"
-                data-original="#000000"
-              ></path>
-            </g>
-          </svg>
-        </button>
-      </section>
+
+      <ticker-graph
+        :graph="graph"
+        :maxGraphElements="maxGraphElements"
+        :selectedTicker="selectedTicker"
+        @close-handler="selectedTicker = null"
+        @change-max-elements-handler="maxGraphElements = $event"
+        @change-graph-handler="graph = $event"
+      />
     </div>
   </div>
 </template>
@@ -237,18 +206,22 @@
 // [x] График сломан если везде одинаковые значения
 // [x] При удалении тикера остается выбор
 
+// [x] TODO: Выделить график в отдельный компонент!
+
 import {
   loadDictionary,
   subscribeToTicker,
   unsubscribeFromTicker,
 } from "./api";
 import AddTicker from "./components/AddTicker.vue";
+import TickerGraph from "./components/TickerGraph.vue";
 
 export default {
   name: "App",
 
   components: {
     AddTicker,
+    TickerGraph,
   },
 
   data() {
@@ -305,18 +278,12 @@ export default {
   // START #15 Криптономикон-4 - Самостоятельная работа (валидации)
   mounted() {
     loadDictionary().then((data) => (this.dictionary = data));
-
-    window.addEventListener(`resize`, this.calculateMaxGraphElements);
   },
   // END #15 Криптономикон-4 - Самостоятельная работа (валидации)
 
-  beforeUnmount() {
-    window.removeEventListener(`resize`, this.calculateMaxGraphElements);
-  },
-
   computed: {
     tooManyTickersAdded() {
-      return this.tickers.length > 4;
+      return this.tickers.length > 20;
     },
 
     startIndex() {
@@ -339,19 +306,6 @@ export default {
       return this.filteredTickers.length > this.endIndex;
     },
 
-    normalizedGraph() {
-      const maxValue = Math.max(...this.graph);
-      const minValue = Math.min(...this.graph);
-
-      if (maxValue === minValue) {
-        return this.graph.map(() => 50);
-      }
-
-      return this.graph.map(
-        (price) => 5 + ((price - minValue) * 95) / (maxValue - minValue)
-      );
-    },
-
     pageStateOptions() {
       return {
         filter: this.filter,
@@ -364,12 +318,6 @@ export default {
     // #21 Криптономикон: улучшаем API - Vue.js: практика
     getFilteredTickersByName(tickerName) {
       return this.tickers.filter((t) => t.name === tickerName);
-    },
-
-    calculateMaxGraphElements() {
-      if (!this.$refs.graph) return;
-      this.maxGraphElements = this.$refs.graph.clientWidth / 38;
-      this.graph = this.graph.slice(-this.maxGraphElements);
     },
 
     updateTicker(tickerName, price) {
@@ -443,10 +391,6 @@ export default {
 
     selectedTicker() {
       this.graph = [];
-
-      this.$nextTick().then(() => {
-        this.calculateMaxGraphElements();
-      });
     },
 
     paginatedTickers() {
